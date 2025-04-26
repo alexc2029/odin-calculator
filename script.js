2fd8033516f6aefa1c055640c7de1997c4359514
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-26 10:45:47 +0300

    Implement clear button, round long decimals, handle premature = and division by 0

--- script.js
+++ script.js
@@ -23,9 +23,6 @@ function divide(a, b) {
 
 function operate(op, a, b) {
 	let result;
-	console.log(a);
-	console.log(op);
-	console.log(b);
 	switch (op) {
 		case "+":
 			result = add(a, b);
@@ -37,17 +34,31 @@ function operate(op, a, b) {
 			result = multiply(a, b);
 			break;
 		case "÷":
-			result = divide(a, b);
+			if (b == 0) alert("Cannot divide by 0!");
+			else {
+				result = divide(a, b);
+			}
+
 			break;
 	}
+	if (!Number.isSafeInteger(result)) result = result.toFixed(2);
 	display.innerText = result;
 	number2 = result;
 	number1 = 0;
 }
 
 function digitClicked(e) {
-	display.innerText += e.target.innerText;
-	number1 = Number(display.innerText);
+	if (e.target.innerText == "C") {
+		number1 = 0;
+		number2 = 0;
+		operator = 0;
+		display.innerText = "";
+	} else {
+		display.innerText += e.target.innerText;
+		number1 = Number(display.innerText);
+	}
+	if (operator == "÷" && number2 && number1 == 0)
+		alert("Cannot divide by 0!");
 }
 
 function operatorClicked(e) {
@@ -58,5 +69,8 @@ function operatorClicked(e) {
 			number2 = number1;
 			number1 = 0;
 		}
-	} else operate(operator, number2, number1);
+	} else if (number1 && number2 && operator) {
+		operate(operator, number2, number1);
+		operator = 0;
+	}
 }
