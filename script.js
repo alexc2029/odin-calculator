6dd81999282f5b8663e73c50ea23404bfc928cfa
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-04-26 11:07:42 +0300

    Fix behavior when new digit is pressed with result displayed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const digits = document.querySelector(".digits");
 const operators = document.querySelector(".operators");
 const display = document.querySelector(".display");
 let number1, number2, operator;
+let resultDisplayed = 0;
 digits.addEventListener("click", digitClicked);
 operators.addEventListener("click", operatorClicked);
 
@@ -43,26 +44,37 @@ function operate(op, a, b) {
 	}
 	if (!Number.isSafeInteger(result)) result = result.toFixed(2);
 	display.innerText = result;
+	resultDisplayed = 1;
 	number2 = result;
 	number1 = 0;
 }
 
+function clear() {
+	number1 = 0;
+	number2 = 0;
+	operator = 0;
+	display.innerText = "";
+}
+
 function digitClicked(e) {
 	if (e.target.innerText == "C") {
-		number1 = 0;
-		number2 = 0;
-		operator = 0;
-		display.innerText = "";
+		clear();
+	} else if (resultDisplayed == 1) {
+		clear();
+		display.innerText = e.target.innerText;
 	} else {
 		display.innerText += e.target.innerText;
-		number1 = Number(display.innerText);
 	}
+	resultDisplayed = 0;
+	number1 = Number(display.innerText);
+
 	if (operator == "÷" && number2 && number1 == 0)
 		alert("Cannot divide by 0!");
 }
 
 function operatorClicked(e) {
 	display.innerText = "";
+	resultDisplayed = 0;
 	if (e.target.innerText != "=") {
 		operator = e.target.innerText;
 		if (!number2) {
